2dcecd79ce628b1dce0ecd3bdfef1dc42f3cc232
masinette <masinette.ts@gmail.com>
2021-03-16 20:37:26 -0400

    add Appointment states to storybook

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,11 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,3 +139,27 @@ storiesOf("Button", module)
           .add("Appointment with Time", () => <Appointment time="12pm" />)
           .add("Header", () => <Header time="12pm" />)
           .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+          .add("Show", () => (
+          <Show 
+            interviewers={interviewers}
+            interviewer = {"Tori Malcolm"}
+            student = "Lydia Miller-Jones"
+            onEdit={action("onEdit")} 
+            onDelete={action("onDelete")}
+          />
+          ))
+          .add("Confirm", () => (
+            <Confirm 
+              message = "Delete the appointment?"
+              onConfirm={action("onConfirm")} 
+              onCancel={action("onCancel")}
+            />
+            ))
+            .add("Status", () => <Status message = "Deleting"/>)
+            .add("Error", () => (
+            <Error 
+              message = "Could not delete appointment"
+              onClose={action("onClose")} 
+              />
+            ))
+            
