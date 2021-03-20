0f24e6c09f9d7f90873b90cdc344026802ab7c88
masinette <masinette.ts@gmail.com>
2021-03-20 15:05:33 -0400

    "applied prettier"

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,34 +1,30 @@
 import React from "react";
 import "components/DayListItem.scss";
 // import { render } from "@testing-library/react";
-const classNames = require ('classnames');
-
+const classNames = require("classnames");
 
 export default function DayListItem(props) {
   const formatSpots = (props) => {
-    if (props === 0){
+    if (props === 0) {
       return "no spots remaining";
     }
-    if (props === 1){
+    if (props === 1) {
       return "1 spot remaining";
-    }
-    else {
+    } else {
       return `${props} spots remaining`;
     }
-  }
+  };
 
   const dayClass = classNames("day-list", {
     "day-list__item": props,
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
-  })
+    "day-list__item--full": props.spots === 0,
+  });
   return (
-    <li className={ dayClass } onClick={() => props.setDay(props.name)} >
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       {/* <h3 className="text--light"> {props.spots} spots remaining</h3> */}
-      <h3 className="text--light"> { formatSpots(props.spots) }</h3>
+      <h3 className="text--light"> {formatSpots(props.spots)}</h3>
     </li>
   );
 }
-
-
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,106 +1,99 @@
 import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 
-
 export default function useApplicationData() {
-
-
   useEffect(() => {
     const daysURL = `http://localhost:8001/api/days`;
-    const appointmentsURL = 'http://localhost:8001/api/appointments';
-    const interviewersURL = 'http://localhost:8001/api/interviewers';
+    const appointmentsURL = "http://localhost:8001/api/appointments";
+    const interviewersURL = "http://localhost:8001/api/interviewers";
 
     Promise.all([
       axios.get(daysURL),
       axios.get(appointmentsURL),
-      axios.get(interviewersURL)
-    ])
-    .then(all => {
+      axios.get(interviewersURL),
+    ]).then((all) => {
       // console.log("DAYS",all[0].data);
       // console.log("APPT", all[1].data);
       // console.log("INTVWR", all[2].data);
-      setState(prev => ({...prev, 
-        days: all[0].data, 
-        appointments: all[1].data, 
-        interviewers: all[2].data
-        }));
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
     });
-  }, [])
-  
-  const setDay = day => setState({ ...state, day });
+  }, []);
 
+  const setDay = (day) => setState({ ...state, day });
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
-
 
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
-  };
-    const newDays = state.days.map(day => {
-      if(state.day === day.name){
-          day.spots--;
+      [id]: appointment,
+    };
+    const newDays = state.days.map((day) => {
+      if (state.day === day.name) {
+        day.spots--;
         // console.log("NEWDAYS SPOTS", day.spots)
       }
       return day;
-    })
-  /*
+    });
+    /*
 - Make the request with appointment id, with the interview data in the body
 - When the response comes back we update the state using the existing setState.
 */
     const bookInterviewURL = `http://localhost:8001/api/appointments/${id}`;
-    return axios.put(bookInterviewURL, {interview})
-    .then(()=>{
-      setState({...state,appointments, newDays}); 
-    })
+    return axios.put(bookInterviewURL, { interview }).then(() => {
+      setState({ ...state, appointments, newDays });
+    });
   }
 
   function cancelInterview(id) {
-    console.log("Clicked trash can")
+    console.log("Clicked trash can");
 
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
     // const newSpots = updateSpots(state.day, state.days, appointments);
-    const newDays = state.days.map(day => {
-      if(state.day === day.name){
-          day.spots++;
-        console.log("NEWDAYS SPOTS", day.spots)
+    const newDays = state.days.map((day) => {
+      if (state.day === day.name) {
+        day.spots++;
+        console.log("NEWDAYS SPOTS", day.spots);
       }
       return day;
-    })
+    });
 
-//     var numbers = [{"myKey":1},{"myKey": 2}];
-// var newarray = numbers.map(myFunction)
-// function myFunction(num) {
-//   num.myKey=num.myKey*10;
-//   return num;
-// }
+    //     var numbers = [{"myKey":1},{"myKey": 2}];
+    // var newarray = numbers.map(myFunction)
+    // function myFunction(num) {
+    //   num.myKey=num.myKey*10;
+    //   return num;
+    // }
 
     //use the appointment id to find the right appointment slot and set it's interview data to null.
- const cancelInterviewURL = `http://localhost:8001/api/appointments/${id}`;
-  return axios.delete(cancelInterviewURL)
-    .then(()=>{
+    const cancelInterviewURL = `http://localhost:8001/api/appointments/${id}`;
+    return axios.delete(cancelInterviewURL).then(() => {
       // console.log("STATE INSIDE CANCEL", state)
-      setState({...state,appointments, newDays});  
+      setState({ ...state, appointments, newDays });
       // console.log( "INSIDE CANCEL", updateSpots(state.day, state.days, state.appointments))
-    })     
+    });
   }
-  return { state, setDay, bookInterview , cancelInterview };
+  return { state, setDay, bookInterview, cancelInterview };
 }
